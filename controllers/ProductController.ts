310927788ca2f6bde66f1cd595b9428b74e2d2cd
Wawan PBS01 <stanlyskwok@gmail.com>
2023-03-22 20:14:06 +0700

    remove errors

--- controllers/ProductController.ts
+++ controllers/ProductController.ts
@@ -1,10 +1,10 @@
 import Product from "../models/product";
 
 class ProductController {
-  static async getAllProducts(req, res, next) {
+  static async getAllProducts(req: any, res: any, next: any) {
     try {
       const data = await Product.getAllProducts();
-      res.status(200).json(data.rows);
+      res.status(200).json(data?.rows);
     } catch (e) {
       console.log(e);
     }
