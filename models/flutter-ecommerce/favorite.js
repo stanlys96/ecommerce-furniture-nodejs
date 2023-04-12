5774a10f34676e2c997f1589124a6e9ecc696dc5
stanlys96 <stanlyskwok@gmail.com>
2023-04-12 08:56:08 +0700

    walao

---
+++ models/flutter-ecommerce/favorite.js
@@ -0,0 +1,38 @@
+const pool = require("../../database/db");
+
+class Favorite {
+  static async getUserFavorites({ user_id }) {
+    try {
+      const favorites = await pool.query(
+        "SELECT * FROM flutter_ecommerce.products p JOIN flutter_ecommerce.user_favorite u ON p.id = u.product_id"
+      );
+      return favorites;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  static async addToFavorite({ user_id, product_id }) {
+    try {
+      const userHasProduct = await pool.query(
+        "SELECT * FROM flutter_ecommerce.user_favorite WHERE user_id = $1 AND product_id = $2",
+        [user_id, product_id]
+      );
+      if (userHasProduct.rowCount > 0) {
+        return {
+          msg: "This product has been added to favorites",
+        };
+      } else {
+        const addToFavorites = await pool.query(
+          "INSERT INTO flutter_ecommerce.user_favorite(user_id, product_id) VALUES($1, $2) RETURNING*;",
+          [user_id, product_id]
+        );
+        return addToFavorites.rows;
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
+
+module.exports = Favorite;
