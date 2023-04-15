307d8d6d810aa9d7adc740717b53ee33af7c17eb
Stanly Sukmajaya Kwok <stanly@Stanlys-MacBook-Pro.local>
2023-04-15 16:41:14 +0700

    walao eh

--- controllers/flutter-ecommerce/UserController.js
+++ controllers/flutter-ecommerce/UserController.js
@@ -13,7 +13,7 @@ class UserController {
   static async register(req, res, next) {
     try {
       const data = await User.register(req.body);
-      res.status(200).json(data.rows);
+      res.status(200).json(data);
     } catch (e) {
       console.log(e);
     }
