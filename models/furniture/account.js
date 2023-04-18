3bf625ebd345e016a8d2d246f8664024b97f43dd
Stanly Sukmajaya Kwok <stanly@Stanlys-MacBook-Pro.local>
2023-04-18 10:34:14 +0700

    walao eh

--- models/furniture/account.js
+++ models/furniture/account.js
@@ -1,12 +1,12 @@
-const dayjs = require('dayjs');
-const pool = require('../../database/db');
-const { hashPassword, comparePassword } = require('../../helpers/bcrypt');
-const { generateToken, verifyToken } = require('../../helpers/jwt');
+const dayjs = require("dayjs");
+const pool = require("../../database/db");
+const { hashPassword, comparePassword } = require("../../helpers/bcrypt");
+const { generateToken, verifyToken } = require("../../helpers/jwt");
 
 class Account {
   static async getAllAccounts() {
     try {
-      const accounts = await pool.query('SELECT * FROM accounts');
+      const accounts = await pool.query("SELECT * FROM accounts");
       return accounts;
     } catch (e) {
       console.log(e);
@@ -27,25 +27,16 @@ class Account {
     try {
       const { username, password, email } = account;
       const findAccount = await pool.query(
-        'SELECT * FROM accounts WHERE email = $1',
+        "SELECT * FROM accounts WHERE email = $1",
         [email]
       );
       if (findAccount.rowCount > 0) {
-        return { msg: 'email_exist' };
+        return { msg: "email_exist" };
       } else {
-        const lastId = await pool.query(
-          'SELECT * FROM accounts ORDER BY user_id DESC LIMIT 1'
-        );
-        let currentId = 0;
-        if (lastId.rowCount > 0) {
-          currentId = lastId.rows[0].user_id + 1;
-        } else {
-          currentId = 1;
-        }
         const hashedPassword = hashPassword(password);
         const newAccount = await pool.query(
-          'INSERT INTO accounts (user_id, username, password, email, created_on) VALUES($1, $2, $3, $4, $5) RETURNING *;',
-          [currentId, username, hashedPassword, email, dayjs().format()]
+          "INSERT INTO accounts (username, password, email, created_on) VALUES($1, $2, $3, $4) RETURNING *;",
+          [username, hashedPassword, email, dayjs().format()]
         );
         return newAccount;
       }
@@ -58,7 +49,7 @@ class Account {
     try {
       const { password, email } = account;
       const findAccount = await pool.query(
-        'SELECT * FROM accounts WHERE email = $1',
+        "SELECT * FROM accounts WHERE email = $1",
         [email]
       );
       if (findAccount.rowCount > 0) {
@@ -67,19 +58,19 @@ class Account {
           findAccount.rows[0].password
         );
         if (!passwordCorrect) {
-          return { msg: 'password_incorrect' };
+          return { msg: "password_incorrect" };
         }
         const token = generateToken(
           { email: findAccount.rows[0].email },
           process.env.SECRET
         );
         return {
-          msg: 'success',
+          msg: "success",
           email: findAccount.rows[0].email,
           token,
         };
       } else {
-        return { msg: 'email_not_found' };
+        return { msg: "email_not_found" };
       }
     } catch (e) {
       console.log(e);
