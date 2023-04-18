416f57838db3fb00241e2c97c7b40591e86786d2
Stanly Sukmajaya Kwok <stanly@Stanlys-MacBook-Pro.local>
2023-04-18 16:50:43 +0700

    walao eh

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const router = require("./routes");
 
 const app = express();
+app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3001;
 
 const corsOptions = {
@@ -12,7 +13,6 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
