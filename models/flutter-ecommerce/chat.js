7d8a699ff81b6ac31666e736208a20517f276d48
stanlys96 <stanlyskwok@gmail.com>
2023-04-22 22:36:33 +0700

    WLAO

--- models/flutter-ecommerce/chat.js
+++ models/flutter-ecommerce/chat.js
@@ -5,7 +5,7 @@ class Chat {
   static async getNormalUserChat({ user_id }) {
     try {
       const chat = await pool.query(
-        'SELECT * FROM flutter_ecommerce.chat WHERE user_id = $1 ORDER BY "TimeStamp";',
+        "SELECT * FROM flutter_ecommerce.chat WHERE user_id = $1 ORDER BY message_sent DESC",
         [user_id]
       );
       return { msg: "success", data: chat.rows };
